04a93ac6036ee76b9cfca2906818a3b4ab58e0ba
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-05-11 20:11:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log("hello"); string.toLowerCase
 function sayHiToGrandma(string){
 
 var lowercase = 'I can\'t hear you!'
- lowercase.toLowerCase() === lowercase // true
+ lowercase.toLowerCase("I can't hear you!") === lowercase // true
 
 return string
 
