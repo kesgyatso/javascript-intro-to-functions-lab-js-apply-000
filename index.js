9335d926e19232c6992e339fe125344b030fa7e6
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-05-11 19:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,12 @@ console.log("hello"); string.toLowerCase
 function sayHiToGrandma(string){
 var uppercase = "HELLO!"
 uppercase.toUpperCase() === uppercase //true
+
+var lowercase = 'hello!'
+ lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase // false
+mixedCase.toUpperCase() === mixedCase // false
+  
 }
