906d8edaf2f44dc02da7e0965068e82e558917a6
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-05-11 20:41:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
 console.log("hello"); string.toLowerCase
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string.lowercase){
 
 var lowercase = 'hello!'
  lowercase.toLowerCase() === string // true
