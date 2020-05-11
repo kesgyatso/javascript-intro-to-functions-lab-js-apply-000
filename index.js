44f4bf2e408516007dd472e3dda66271eb7f1e71
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-05-11 19:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string){
   return string.toLowerCase()
 }
+
 function logShout(string){
  console.log ("HELLO"); string.toUpperCase
 }
+
 function logWhisper(string){
 console.log("hello"); string.toLowerCase
 }
 
 function sayHiToGramdma(string){
-  
+return string.lowerCase  
 }
